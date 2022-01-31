4664191ac0907002bbbfe7f0f46d0bb96c521f5e
Chen <jonkiky@gmail.com>
2022-01-31 11:48:39 -0500

    refine elasticsearch scripts

--- elasticsearch/env.sh
+++ elasticsearch/env.sh
@@ -1,3 +1,8 @@
-export PREFIX="path to file"
+# export PREFIX="path to file"
+export PREFIX="/home/bento/mtp-data-files/mtp-data-files/OT_21.06"
+
+# export ES=localhost:9200
 export ES=localhost:9200
-export INDEX_SETTINGS="path to index_settings.json"
+
+# export INDEX_SETTINGS="path to index_settings.json"
+export INDEX_SETTINGS="/home/bento/ppdc-dataloader/elasticsearch/index_settings.json"
