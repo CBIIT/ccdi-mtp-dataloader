ac3e966f71beb793dc8b21a8449da167c743402a
Chen <jonkiky@gmail.com>
2022-02-09 16:04:58 -0500

    update the mapping for chop_data

--- elasticsearch/load_chop_nav_data.sh
+++ elasticsearch/load_chop_nav_data.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-export INDEX_SETTINGS="index_settings_chop_nav_data.json"
+# export INDEX_SETTINGS="path to index_settings.json"
+export INDEX_SETTINGS=""
+
+
 
 export INDEX_NAME="chop_data"
 export INPUT="${PREFIX}/chop_data"
